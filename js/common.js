e2c5b97c81a86f7564af28d8ee8e4cbf14203351
Artur <morozarturv@gmail.com>
2013-01-22 18:45:34 +0200

    main 80%

--- js/common.js
+++ js/common.js
@@ -1,18 +1,15 @@
 $(document).ready(function() {
 
-//select
-$('.popup__select span').click(function() {
-	$(this).next().slideDown(200);
-});
-$('.popup__select li').click(function() {
-	var sel_val = $(this).text();
-	$(this).parent().prev().html(sel_val+'<i></i>');
-	$(this).parent().slideUp(200);
-});
-
-//go-top
-$('.go-top').click(function() {
-	$('body').scrollTo(0, 600);
+//profile
+$('.user__mail button').click(function() {
+	if ($(this).parent().parent().hasClass('active')) {
+		$(this).parent().parent().removeClass('active');
+		$(this).parent().next().hide();
+	}
+	else {
+		$(this).parent().parent().addClass('active');
+		$(this).parent().next().show();
+	}
 });
 
 });
