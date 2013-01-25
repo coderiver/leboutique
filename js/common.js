08a7a493376dcd05cd646e6165f8bd6a1ea520d0
Artur <morozarturv@gmail.com>
2013-01-25 18:17:48 +0200

    post

--- js/common.js
+++ js/common.js
@@ -1,15 +1,37 @@
 $(document).ready(function() {
 
-//profile
-$('.user__mail button').click(function() {
-	if ($(this).parent().parent().hasClass('active')) {
-		$(this).parent().parent().removeClass('active');
-		$(this).parent().next().hide();
+//window width
+function window_width() {	
+	var wnd_width = $(window).width();
+	if (wnd_width < 1280) {
+		$('.action_big').css('width', '100%');
+		$('.action_small:first').css('width', '33.3333%');
+		$('.action_small:first').next().css('width', '33.3333%');
 	}
 	else {
-		$(this).parent().parent().addClass('active');
-		$(this).parent().next().show();
+		$('.action_big').css('width', '50%');
+		$('.action_small:first').css('width', '25%');
+		$('.action_small:first').next().css('width', '25%');
+	}	
+};
+
+//init functions
+window_width();
+//window resize
+$(window).resize(function () {
+	window_width();
+});
+
+//profile
+$('.user__mail').click(function() {
+	if ($(this).parent().hasClass('active')) {
+		$(this).parent().removeClass('active');
+		$(this).next().hide();
 	}
+	else {
+		$(this).parent().addClass('active');
+		$(this).next().show();
+	};
 });
 
 });
